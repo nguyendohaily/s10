aa649719d75aeeadb0c2b4a6a297617e67d76196
nguyendohaily <nguyendohaily@gmail.com>
2026-07-23 11:07:36 +0700

    s10-btvn-3

---
+++ s10-btvn-3/s10-btvn-3.js
@@ -0,0 +1,8 @@
+let width = 5;  
+let height = 10; 
+
+let area = width * height;
+
+document.write("Chiều rộng: " + width + "<br>");
+document.write("Chiều cao: " + height + "<br>");
+document.write("Diện tích hình chữ nhật (area): " + area);
